68769f427452e5971c10bf3a0327a07d3d5fdd81
sebanma <Sebas13david21@gmail.com>
2022-08-13 01:00:13 -0500

    Ejercicios(Solución Final)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Taller-Numero-2
+
+Para el punto numero 1, observamos 2 botones uno para agregar la palabra o frase que se ingrese en el campo denominado 'Ingrese Texto', en la parte inferior esta la lista, es el lugar donde va apareciendo el texto en miniscula sin importar que se ingrese en mayuscula y por ultimo tenemos el boton para realizar limpieza.
+
+Para el Punto numero 2 del Cronometro, observamos 3 botones, el boton iniciar, Detener y reiniciar, por lo tanto el codigo javascript fue implementado para introducir diferentes funcionalidades a estos botones.
--- Taller Ejercicio 1/js/js.js
+++ Taller Ejercicio 1/js/js.js
@@ -1,7 +1,16 @@
 window.onload = function () {
-   listar();
-}
-
-listar(){
-
-}
+    const agregar = document.getElementById("boton1");
+    const borrar = document.getElementById("boton2");
+    var lista = document.getElementById("lista");
+  
+    agregar.onclick = function () {
+      var text = document.getElementById("msg");
+      var li = lista.getElementsByTagName("li");
+      var opcion = "<li>" + text.value.toLowerCase() + "</li>";
+      lista.innerHTML += opcion;
+    };
+  
+    borrar.onclick = function() {
+      lista.innerHTML = "";
+    }
+  };
